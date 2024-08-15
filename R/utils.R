a4b4686d8bf20106aa4607d072f74e3c9a5e5208
Ryan Field <RyanJField@users.noreply.github.com>
2024-08-15 16:06:38 +0100

    Helper functions

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,25 @@ get_missing_variables <- function(
   }
   return(.missing_variables)
 }
+
+binary_to_df <- function(
+  x
+) {
+  `%>%` <- magrittr::`%>%`
+  binary_df <- as.data.frame(x$binary_variables)
+  binary_df <- binary_df %>%
+    tidyr::pivot_longer(
+      cols = tidyr::everything(),
+      names_to = "variable",
+      values_to = "mean"
+    )
+  return(binary_df)
+}
+
+quantiles_to_df <- function(
+  x
+) {
+  .quantiles <- lapply(x, \(.x) .x[["quantiles"]])
+  .quantiles <- do.call(rbind, .quantiles)
+  return(.quantiles)
+}
