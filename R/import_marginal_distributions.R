925e90ad1176a0154a702a02416fb6638da7de90
Ryan J Field <RyanJField@users.noreply.github.com>
2024-08-22 22:04:35 +0100

    Updates

--- R/import_marginal_distributions.R
+++ R/import_marginal_distributions.R
@@ -11,24 +11,50 @@ import_marginal_distributions <- function(
       "Directory must exist, hint: set create_folder to TRUE"
     )
   }
-  .binary_variables_file <- get_variables_path(
-    folder_path,
-    binary_variables_file,
+
+  .binary_variables <- load_variables_file(
+    get_variables_path(
+      folder_path,
+      binary_variables_file,
+      "binary"
+    ),
     "binary"
   )
-  .categorical_variables_file <- get_variables_path(
-    folder_path,
-    categorical_variables_file,
+
+  .categorical_variables <- load_variables_file(
+    get_variables_path(
+      folder_path,
+      categorical_variables_file,
+      "categorical"
+    ),
     "categorical"
   )
-  .continuous_variables_file <- get_variables_path(
-    folder_path,
-    continuous_variables_file,
+
+  .continuous_variables <- load_variables_file(
+    get_variables_path(
+      folder_path,
+      continuous_variables_file,
+      "continuous"
+    ),
     "continuous"
   )
-  .continuous_quantiles_file <- get_variables_path(
-    folder_path,
-    continuous_quantiles_file,
+
+  .quantile_variables <- load_variables_file(
+    get_variables_path(
+      folder_path,
+      continuous_quantiles_file,
+      "quantiles"
+    ),
     "quantiles"
   )
+
+  if (! is_variables_valid(
+    .binary_variables,
+    .categorical_variables,
+    .continuous_variables,
+    .quantile_variables
+  )) {
+    stop("The input files are not valid for the RESIDE package")
+  }
+
 }
