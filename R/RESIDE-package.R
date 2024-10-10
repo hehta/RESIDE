9b5184114c937569e0baacf796800c9f8da27bd1
Ryan Field <RyanJField@users.noreply.github.com>
2024-10-10 17:04:14 +0100

    Add funding to documentation

--- R/RESIDE-package.R
+++ R/RESIDE-package.R
@@ -2,6 +2,12 @@
 #' `r lifecycle::badge("experimental")`
 #'
 #' The RESIDE Package
+#'
+#' This work was supported by the UKRI Strength in Places Fund (SIPF)
+#' Competition, #' project number 107140.
+#' The project title is SIPF The Living Laboratory driving economic growth
+#' in Glasgow through real world implementation of precision medicine.
+#'
 #' @importFrom magrittr "%>%"
 #' @importFrom dplyr mutate
 #' @importFrom dplyr across
